2ca1475b19e50b35e0be5fa9cf06ba3f4d162545
Brian Wardwell <bwardwell79@gmail.com>
2020-11-24 14:43:10 -0800

    separated index.js and server.js, overall restructure for Express Router refactor

---
+++ api/server.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const helpers = require("../models/notesHelpers");
+
+const server = express();
+
+server.use(express.json());
+
+//Basic routing structure:
+//server.METHOD(PATH, HANDLER)
+//server is an instance of Express
+//Method is an http request
+//Path is a path on the server
+//Handler is the function executed when the route is matched
+
+server.get("/api/notes", (req, res) => {
+  helpers
+    .find()
+    .then((notes) => {
+      res.json(notes);
+    })
+    .catch((err) => {
+      res.status(500).json({ error: "couldn't retrieve notes" });
+    });
+  // res.json(notes)
+});
+
+server.get("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+
+  helpers
+    .findById(id)
+    .then((found) => {
+      if (found !== undefined) {
+        res.status(200).json(found);
+      } else res.status(500).json({ message: "ID doesn't exist" });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "couldnt find that ID" });
+    });
+});
+
+server.post("/api/notes", (req, res) => {
+  helpers
+    .add(req.body)
+    .then((note) => {
+      res.status(200).json(note);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: "Failed to create the note" });
+    });
+});
+
+server.put("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  helpers
+    .update(id, changes)
+    .then((updated) => {
+      res
+        .status(200)
+        .json({ message: `Successfully update note # ${updated}` });
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Couldn't update note" });
+    });
+});
+
+server.delete("/api/notes/:id", (req, res) => {
+  const { id } = req.params;
+  helpers
+    .remove(id)
+    .then((count) => {
+      if (count > 0) {
+        res
+          .status(200)
+          .json({ message: `successfully deleted note # ${count}` });
+      } else {
+        res.status(404).json({ message: "Unable to find that record" });
+      }
+    })
+    .catch(() => {
+      res.status(500).json({ message: "Server error" });
+    });
+});
+
+module.exports = server;
---
+++ index.js
@@ -0,0 +1,5 @@
+//index.js needs 1.) access to the server file, 2.) a defined port, 3.) listen function
+const server = require("./api/server")
+const port = 5000;
+
+server.listen(port, () => console.log(`Server started on port ${port}`));

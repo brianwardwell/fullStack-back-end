031d21a1425488e42aca9ca50b23a267c1e2f6b4
Brian Wardwell <bwardwell79@gmail.com>
2020-12-07 15:40:59 -0800

    migrated users table

--- Routes/users-router.js
+++ Routes/users-router.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const useHelp = require('../models/usersHelpers')
-const router = express.Router()
+const express = require("express");
+const useHelp = require("../models/usersHelpers");
+const router = express.Router();
 
-router.post('/')
+router.post("/", (req, res) => {
+    console.log("Req Body!", req.body)
+  useHelp
+    .addUser(req.body)
+    .then((added) => {
+      res.status(200).json(added);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
+});
 
 module.exports = router;

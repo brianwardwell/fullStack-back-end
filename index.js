6d11c9aa201ff1f3de761b9eb5fda501f7545930
Brian Wardwell <bwardwell79@gmail.com>
2021-02-09 16:45:18 -0800

    renamed .env port for deployment

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 //index.js needs 1.) access to the server file, 2.) a defined port, 3.) listen function
 const server = require("./API/server")
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server started on port ${port}`));
-
-// server.get("/api/users", (req, res) => {
-//     res.json({message: "Users endpdoint functional"})
-// });

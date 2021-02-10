2981b32e8eec433f8ed0c0cd298e19a293ddad64
Brian Wardwell <bwardwell79@gmail.com>
2021-02-09 17:18:03 -0800

    fixed package json main directory

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //index.js needs 1.) access to the server file, 2.) a defined port, 3.) listen function
-const server = require("./API/server")
+const server = require("./api/server")
 const port = process.env.PORT || 5000;
 
 server.listen(port, () => console.log(`Server started on port ${port}`));

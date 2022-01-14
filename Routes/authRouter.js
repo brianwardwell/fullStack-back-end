1a263c6165d0173a9f4f183e5eb65135f7fed4a0
brianwardwell <brian.wardwell@datadoghq.com>
2022-01-13 18:27:11 -0800

    registration error changed

--- Routes/authRouter.js
+++ Routes/authRouter.js
@@ -22,7 +22,7 @@ router.post("/signIn", (req, res) => {
       res.status(200).json({ message: `added user ${added}` });
     })
     .catch((err) => {
-      res.status(500).json({ message: err.message });
+      res.status(500).json({ message: "Couldn't register that person bruh" });
     });
 });
 
